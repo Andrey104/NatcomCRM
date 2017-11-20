aa667b11122d688dc071cc530bb92fd494e4cfa1
andrey104 <andreyrs104@gmail.com>
2017-11-20 19:39:26 +0300

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { MainComponent } from './main/main.component';
+import { LeadComponent } from './lead/lead.component';
+import { OrderPageComponent } from './order-page/order-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent,
+    MainComponent,
+    LeadComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './main.ts',
+  output: {
+    filename: 'bundle.js'
+  }
+};
