8ef843b814288dd6e08d54999d980a9afa4476aa
PaulCardLord <paulkapay@gmail.com>
2017-12-13 21:16:21 +0300

    add deal comment

---
+++ src/app/deal-page/deal-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deal-page',
+  templateUrl: './deal-page.component.html',
+  styleUrls: ['./deal-page.component.css']
+})
+export class DealPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
