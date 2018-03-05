61d86e8c966153c60bedcbe514e5a6b4eac7659a
andrey104 <andreyrs104@gmail.com>
2018-03-05 21:13:37 +0300

    add modal open function dynamic subscribe component

--- src/app/admin/settings-page/settings-page.component.ts
+++ src/app/admin/settings-page/settings-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit} from '@angular/core';
+import {Component, ComponentRef, EventEmitter, OnInit} from '@angular/core';
 import {OrderService} from '../../services/order.service';
 import {InstallersService} from '../../services/installers.service';
 import {InstallerPosition} from '../../models/installers/installer_position';
@@ -6,6 +6,8 @@ import {log} from 'util';
 import {Installer} from '../../models/installers/installer';
 import {InstallersPage} from '../../models/installers/installers_page';
 import {Observable} from 'rxjs/Observable';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-installers-page',
@@ -14,17 +16,31 @@ import {Observable} from 'rxjs/Observable';
 })
 export class SettingsPageComponent implements OnInit {
   modalIsOpen = false;
+  private subscriptions: Subscription[] = [];
   constructor() {
   }
 
   ngOnInit() {
   }
-  onActivate(event: EventEmitter<number>) {
-    alert('activate');
+  /* c- component instance
+   * onActivate is called when router changes
+   * onActivate subscribe to change modalIsOpen
+   * onDeactivate unsubscribe all subscription
+   */
+  onActivate(c) {
+    const modal = c.modal
+      .subscribe(next => {
+        if (next) {
+          this.modalIsOpen = true;
+        } else {
+          this.modalIsOpen = false;
+        }
+      });
+    this.subscriptions.push(modal);
   }
-  onDeactivate(event: EventEmitter<number>) {
-    alert('deactivate');
+  onDeactivate(c) {
+    this.subscriptions
+      .forEach(s => s.unsubscribe());
   }
-
 
 }
