a00b1b761499c649471abc25a9fc59f0f505ace6
Denis Kharlamov <dharlamov97@gmail.com>
2018-04-06 22:37:57 +0300

    modified new-event component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {WebsocketService} from './services/websocket.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,15 @@ import {WebsocketService} from './services/websocket.service';
 export class AppComponent {
   orderId: number;
   showEvent = false;
+  subOnWebSocket: Subscription;
 
   constructor(private webSocketService: WebsocketService) {
     this.webSocketService.makeSocketConnection();
+    this.subOnWebSocket = this.webSocketService.message.subscribe((response) => {
+      if (response.event === 'on_create_order') {
+        this.orderId = response.data.order_id;
+        this.showEvent = true;
+      }
+    });
   }
 }
