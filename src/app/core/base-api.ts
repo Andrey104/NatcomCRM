c0c02a9d629b8363979c0234526cb333d43c257a
andrey104 <andreyrs104@gmail.com>
2018-04-07 16:14:05 +0300

    Merge remote-tracking branch 'remotes/origin/deal_mount'
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/core/base-api.ts
+++ src/app/core/base-api.ts
@@ -4,7 +4,7 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class BaseApi {
-  private baseUrl = 'http://188.225.46.31/api/';
+  private baseUrl = '/api/';
 
   constructor(public http: HttpClient) {
   }
